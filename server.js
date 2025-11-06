a44577c43783529e587f74cd9da2c9daa0ea4502
CasualR <kolyabaner@gmail.com>
2025-11-06 22:00:37 +0100

    second commit

--- server.js
+++ server.js
@@ -2,4 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = 8383
 
+// Website endpoints
+
+app.get('/', (req, res) => {
+    res.send('<h1>Homepage</h1>')
+})
+
+app.get('/dashboard', (req, res) => {
+    console.log('I hit the /dashboard endpoint')
+    res.send('hi')
+})
+
+// API endpoints (non-visual)
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
