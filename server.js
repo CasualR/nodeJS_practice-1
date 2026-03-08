fd413e2c779749716898e89ef7b3cab4e18ed404
CasualR <kolyabaner@gmail.com>
2026-03-08 22:21:47 +0100

    no war

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let data = ['Mykola', 'Jake']
 // Middleware
 app.use(express.json())
 
-// practice and more practice more and more love more and more make love no war
+// practice and more practice more and more love more and more make love no war no war
 
 // Website endpoints (visual)
 
